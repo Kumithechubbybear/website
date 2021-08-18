3594d1acc5097bcb9ba02279004d101aecc8d2a0
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-18 20:58:14 +0700

    📃Create more page with <link />

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,12 @@ import '../public/css/App.css'
 
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Homepage = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
         <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
@@ -42,6 +35,133 @@ function App() {
       </header>
     </div>
   )
-}
-
-export default App
+};
+const Productpage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <p>Hello Vite + React!</p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+};
+const Orderpage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <p>Hello Vite + React!</p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+};
+const Contactpage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <p>Hello Vite + React!</p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+};
+const Donatepage = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+        <p>Hello Vite + React!</p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+};
+export {Homepage, Productpage,Orderpage,Contactpage,Donatepage }; 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react' ;
+import ReactDOM from 'react-dom' ;
+import './index.css' ;
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {Homepage, Productpage,Orderpage,Contactpage,Donatepage} from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <Router>
+       <Switch>
+		      <Route exact path="/" component={Homepage}/>
+			    <Route exact path="/Product" component={Productpage}/>
+          <Route exact path="/Order" component={Orderpage}/>
+          <Route exact path="/Contact" component={Contactpage}/>
+          <Route exact path="/Donate" component={Donatepage}/>
+	    </Switch>
+    </Router>,
+    document.getElementById('root')
+);
+
