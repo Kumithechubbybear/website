6dd37908dd4d1d6732d43050c02b9b7d7d455459
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-21 13:48:45 +0700

    Create donate page

--- src/App.tsx
+++ src/App.tsx
@@ -99,6 +99,8 @@ const Contactpage = () => {
         <div className="text-2xl text-gray-400 font-medium font-sans text-opacity-75">
         320 ถนนพระรามที่ ๔ แขวง มหาพฤฒาราม เขตบางรัก กรุงเทพมหานคร 10500
         </div>
+        <br /><br />
+        
       </header>
     </div>
   )
@@ -108,31 +110,40 @@ const Donatepage = () => {
     <div className="App">
       <header className="App-header">
         <Nav5 />
-        <p>Hello Vite + React!</p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <div className="text-7xl font-bold text-pink-500">
+        Donate
+        </div>
+        <br /><br />
+        <div className="text-3x1">
+          We have many platform that you can donate
+        </div>
+        <div className="text-gray-700 body-font overflow-hidden bg-white">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="lg:w-4/5 mx-auto flex flex-wrap">
+    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Donate on ko-fi</h1>
+        <div className="flex mb-4">
+
+        </div>
+        <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+
+
+      </div>
+      <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Donate on paypal</h1>
+        <div className="flex mb-4">
+
+        </div>
+        <p className="leading-relaxed">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean shorts keytar banjo tattooed umami cardigan.</p>
+
+
+      </div>
+    </div>
+  </div>
+  </div>
       </header>
     </div>
   )
 };
+
 export {Homepage, Productpage,Orderpage,Contactpage,Donatepage }; 
